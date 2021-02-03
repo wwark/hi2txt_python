bed8a19e6309b95ec57c8b50c1eafd882206d0c8
Wark <wark@free.fr>
2021-02-03 23:44:15 +0100

    Support Rampage NVRAM

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ example : python3 hi2txt_python.py robotron.xml robotron.nvram
 | atlantis2 | :heavy_check_mark: |
 | calipso | :x: |
 | galaxian | :heavy_check_mark: |
-| rampage | :x: |
+| rampage | :heavy_check_mark: |
 | robotron | :x: |
 | scramble | :heavy_check_mark: |
 | tazmania | :x: |
--- hi2txt_python.py
+++ hi2txt_python.py
@@ -39,7 +39,7 @@ def build_structure():
 					for x in range(range_start, range_end):
 						datas = {}
 						datas['LOOP'] = 'LOOP_' + structure_child[0].attrib['id']
-						datas['INC'] = str(x)
+						datas['INDEX'] = str(x)
 						for loop_child in structure_child:
 							datas_loop = []
 							if loop_child.tag == 'elt':
@@ -97,6 +97,12 @@ def convert_value_from_bytes(output_id):
 def format_converted_value(value_to_format, format_value):
 
 	final_value = 'VALUE_TO_FORMAT'
+	list_format_value = format_value.split(';')
+	if len(list_format_value) > 1:
+		if list_format_value[0] == '+1':
+			value_to_format = value_to_format + 1
+			format_value = list_format_value[1]
+
 	for child2 in root:
 		if child2.tag == 'format':
 			if format_value in child2.attrib['id']:
@@ -114,93 +120,102 @@ def format_converted_value(value_to_format, format_value):
 
 def print_fields():
 
-	for child in root:
-		if child.tag == 'output':
-			for output_child in child:
-				format_value = ''
-				if output_child.tag == 'field':
-					if 'format' in output_child.attrib:
-						format_value = output_child.attrib['format']
+	for output_child_datas in root:
+		if output_child_datas.tag == 'output':
+			for field_child_datas in output_child_datas:
+				if field_child_datas.tag == 'field':
+					datas_child_field = []
+					format_value = ''
+					if 'format' in field_child_datas.attrib:
+						format_value = field_child_datas.attrib['format']
 					for x in  data_structure:
-						if output_child.attrib['id'] in str(x):
-							datas_child_field = []
-							datas_child_field.append(output_child.attrib['id'])
+						if field_child_datas.attrib['id'] in str(x):
+							datas_to_append = {}
 							working_value = ''
 							working_value = convert_value_from_bytes(str(x))
 							if format_value != '':
 								working_value = format_converted_value(working_value, format_value)
-							datas_child_field.append(working_value)
+							datas_to_append[field_child_datas.attrib['id']] = working_value
+							datas_child_field.append(datas_to_append)
 					if len(datas_child_field) > 0:
 						print(datas_child_field)
 
 
-def print_table_fields():
+def print_tables():
 	
-	for child in root:
-		if child.tag == 'output':
-			for output_child in child:
-				format_value = ''
-				if output_child.tag == 'table':
-					title_child_table = []
-					datas_child_table_field = []
-					for table_child in output_child:
-						if 'format' in output_child.attrib:
-							format_value = output_child.attrib['format']
-						if table_child.tag == 'field':
-							title_child_table.append(table_child.attrib['id'])
-							for x in  data_structure:
-								if table_child.attrib['id'] in str(x):
-									working_value = ''
-									working_value = convert_value_from_bytes(str(x))
-									if format_value != '':
-										working_value = format_converted_value(working_value, format_value)
-									datas_child_table_field.append(working_value)
-							
-					if len(title_child_table) > 0:
-						print(title_child_table)
-					if len(datas_child_table_field) > 0:
-						print(datas_child_table_field)
-
-
-def print_table_columns():
-
-	for child in root:
-		if child.tag == 'output':
-			for output_child in child:
-				format_value = ''
-				if output_child.tag == 'table':
-					title_child_table = []
-					datas_child_table_column = []
-					for table_child in output_child:
-						if 'format' in output_child.attrib:
-							format_value = output_child.attrib['format']
-						if table_child.tag == 'column':
-							title_child_table.append(table_child.attrib['id'])
-							for datas_values in data_structure_loop:
-								if 'LOOP_' + table_child.attrib['id']  in str(datas_values):
-									datas_child_table_column.append(datas_values)
-
-					datas_child_table_column_print = []
-					for values_column_datas in datas_child_table_column:
-						datas = []
-						for title_column in title_child_table:
-							if title_column == 'RANK':
-								# continue
-								datas.append(int(values_column_datas['INC']) + 1)
-							else:
-								for key_column in values_column_datas:
-									if key_column == 'VALUES_' + title_column + '_' + values_column_datas['INC']:
+	for output_child_datas in root:
+		if output_child_datas.tag == 'output':
+			for table_child_datas in output_child_datas:
+				if table_child_datas.tag == 'table':
+					if table_child_datas[0].tag == 'field':
+						title_child_table = []
+						datas_child_table_fields = []
+						for field_child_datas in table_child_datas:
+							if field_child_datas.tag == 'field':
+								format_value = ''
+								if 'format' in field_child_datas.attrib:
+									format_value = field_child_datas.attrib['format']
+								title_child_table.append(field_child_datas.attrib['id'])
+								for x in  data_structure:
+									if field_child_datas.attrib['id'] in str(x):
 										working_value = ''
-										working_value = convert_value_from_bytes(str(values_column_datas[key_column][0]))
+										working_value = convert_value_from_bytes(str(x))
 										if format_value != '':
 											working_value = format_converted_value(working_value, format_value)
-										datas.append(working_value)
-						datas_child_table_column_print.append(datas)
-					
-					if len(title_child_table) > 0:
-						print(title_child_table)
-					for toto in datas_child_table_column_print:
-						print(toto)
+										datas_child_table_fields.append(working_value)
+
+						if len(title_child_table) > 0:
+							print(title_child_table)
+						if len(datas_child_table_fields) > 0:
+							print(datas_child_table_fields)
+
+					elif table_child_datas[0].tag == 'column':
+						title_child_table = []
+						title_child_table_info_column = []
+						datas_child_table_columns = []
+						data_structure_loop_values_per_loop = []
+						for column_child_datas in table_child_datas:
+							if column_child_datas.tag == 'column':
+								format_column = 'None'
+								if 'format' in column_child_datas.attrib:
+									format_column = column_child_datas.attrib['format']
+								is_index_column = False
+								if 'src' in column_child_datas.attrib and column_child_datas.attrib['src'] == 'index':
+									is_index_column = True
+								name_column = column_child_datas.attrib['id']
+								info_column = {}
+								info_column['name_column'] = name_column
+								info_column['is_index_column'] = is_index_column
+								info_column['format_column'] = format_column
+								title_child_table.append(name_column)
+								title_child_table_info_column.append(info_column)
+								# identified the loop
+								for data_structure_loop_values in data_structure_loop:
+									if 'LOOP_' + column_child_datas.attrib['id']  in str(data_structure_loop_values):
+										data_structure_loop_values_per_loop.append(data_structure_loop_values)
+
+						for data_structure_loop_values_per_loop_values in data_structure_loop_values_per_loop:
+							datas = []
+							for title_child_table_info_column_values in title_child_table_info_column:
+								if title_child_table_info_column_values['is_index_column']:
+									working_value = int(data_structure_loop_values_per_loop_values['INDEX'])
+									if title_child_table_info_column_values['format_column'] != 'None':
+										working_value = format_converted_value(working_value, title_child_table_info_column_values['format_column'])
+									datas.append(working_value)
+								else:
+									for key_column in data_structure_loop_values_per_loop_values:
+										if key_column == 'VALUES_' + title_child_table_info_column_values['name_column'] + '_' + data_structure_loop_values_per_loop_values['INDEX']:
+											working_value = ''
+											working_value = convert_value_from_bytes(str(data_structure_loop_values_per_loop_values[key_column][0]))
+											if title_child_table_info_column_values['format_column'] != 'None':
+												working_value = format_converted_value(working_value, title_child_table_info_column_values['format_column'])
+											datas.append(working_value)
+							datas_child_table_columns.append(datas)
+
+						if len(title_child_table) > 0:
+							print(title_child_table)
+						for datas_child_table_columns_print in datas_child_table_columns:
+							print(datas_child_table_columns_print)
 
 
 # Start Program
@@ -236,6 +251,4 @@ data_structure_loop = []
 build_structure()
 # Print in the console
 print_fields()
-print_table_fields()
-print_table_columns()
-# print(data_structure_loop)
+print_tables()
